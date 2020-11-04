cef2e7c20ff39aa06b04f2fadcaf92761e0149e2
SatriaPermana21 <73052087+SatriaPermana21@users.noreply.github.com>
2020-11-04 23:08:27 +0700

    Create tugas6-lab2.py

---
+++ tugas6-lab2.py
@@ -0,0 +1,11 @@
+a = input("Masukkan Nilai a = ")
+b = input("Masukkan Nilai b = ")
+print("Variable a = ", a)
+print("Variable b = ", b)
+print("Hasil penggabungan {0} & {1} = %s".format(a,b) % (a+b))
+
+# Konversi nilai variable ke dalam integer
+a = int(a)
+b = int(b)
+print("Hasil penjumlahan {0} + {1} = %d".format(a,b) % (a+b))
+print("Hasil pembagian {0} / {1} = %d".format(a,b) % (a/b))
